75c46d41ce01b6b8747cab468e591b46545ad316
David <ns.erosdavid@gmail.com>
2022-09-18 18:37:12 +0200

    feat: added support for dates for comments and posts; some additional sql entries

--- src/main/java/com/dislinkt/postservice/dto/CommentDTO.java
+++ src/main/java/com/dislinkt/postservice/dto/CommentDTO.java
@@ -6,12 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class CommentDTO {
     private String commentText;
     private String username;
     private Long userPostId;
+    private Date commentDate;
+
+    public CommentDTO(){
+        this.commentDate = new Date();
+    }
 }
--- src/main/java/com/dislinkt/postservice/service/CommentService.java
+++ src/main/java/com/dislinkt/postservice/service/CommentService.java
@@ -2,7 +2,9 @@ package com.dislinkt.postservice.service;
 
 
 import com.dislinkt.postservice.model.Comment;
+import com.dislinkt.postservice.model.UserPost;
 import com.dislinkt.postservice.repository.CommentRepository;
+import com.dislinkt.postservice.repository.UserPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +17,20 @@ public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    @Autowired
+    private UserPostRepository userPostRepository;
+
     public Comment saveComment(@RequestBody Comment comment){
         return commentRepository.save(comment);
     }
 
     public List<Comment> getAllComments(){
         return commentRepository.findAll();
+    }
+
+    public UserPost getPostById(Long id){
+        return userPostRepository.findById(id).get();
+
     }
 
 
