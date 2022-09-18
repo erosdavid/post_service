38003ce5bc4174879db8005aa53b4173247cbf51
David <ns.erosdavid@gmail.com>
2022-09-18 21:00:27 +0200

    feat: changes in controller

--- src/main/java/com/dislinkt/postservice/controller/UserPostController.java
+++ src/main/java/com/dislinkt/postservice/controller/UserPostController.java
@@ -6,15 +6,13 @@ import com.dislinkt.postservice.model.Comment;
 import com.dislinkt.postservice.model.UserPost;
 import com.dislinkt.postservice.service.UserPostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserPostController {
     @Autowired
     private UserPostService userPostService;
