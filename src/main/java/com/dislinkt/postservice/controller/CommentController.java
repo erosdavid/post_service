12f97bbbb00dad8235d5e0cbaa2361f56f56f33f
milanjokanovic <milan.jokanovic98@gmail.com>
2023-01-22 12:21:32 +0100

    feat: Added monitoring functions to service

--- src/main/java/com/dislinkt/postservice/controller/CommentController.java
+++ src/main/java/com/dislinkt/postservice/controller/CommentController.java
@@ -3,6 +3,8 @@ package com.dislinkt.postservice.controller;
 import com.dislinkt.postservice.dto.CommentDTO;
 import com.dislinkt.postservice.model.Comment;
 import com.dislinkt.postservice.service.CommentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +17,11 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
+
     @PostMapping("/saveComment")
     public Comment saveComment(@RequestBody CommentDTO commentDTO){
+        LOGGER.info("Saving new comment on post " + commentDTO.getUserPostId() + " from user " + commentDTO.getUsername());
         Comment comment = new Comment();
         comment.setCommentText(commentDTO.getCommentText());
         comment.setUsername(commentDTO.getUsername());
@@ -28,6 +33,8 @@ public class CommentController {
 
     @GetMapping("/getAllComments")
     public List<Comment> getAllComments(){
+
+        LOGGER.info("Getting all comments");
         return commentService.getAllComments();
     }
 }
--- src/main/java/com/dislinkt/postservice/controller/DislikePostController.java
+++ src/main/java/com/dislinkt/postservice/controller/DislikePostController.java
@@ -4,6 +4,8 @@ import com.dislinkt.postservice.dto.DislikePostDTO;
 import com.dislinkt.postservice.model.DislikePost;
 import com.dislinkt.postservice.model.LikePost;
 import com.dislinkt.postservice.service.DislikePostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +20,21 @@ public class DislikePostController {
     @Autowired
     private DislikePostService dislikePostService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DislikePostController.class);
+
     @PostMapping("/saveDislikePost")
     public int saveDislikePost(@RequestBody DislikePostDTO dislikePostDTO){
+
+        LOGGER.info("Adding dislike to post " + dislikePostDTO.getUserPostID() + " by user " + dislikePostDTO.getUsername());
         return dislikePostService.saveDislikePost(dislikePostDTO);
     }
 
     @DeleteMapping("/deleteDislikePost/{id}")
     public ResponseEntity<?> deleteDislikePost(@PathVariable Long id){
+        LOGGER.info("Removing dislike on post " + id);
         DislikePost delete = dislikePostService.deleteDislikePost(id);
         if (delete != null){
+            LOGGER.info("Removing dislike on post " + id + " failed");
             return ResponseEntity.status(HttpStatus.OK).body(delete);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(delete);
@@ -34,6 +42,7 @@ public class DislikePostController {
 
     @GetMapping("/getAllDislikePosts")
     public List<DislikePost> getAllDislikePosts(){
+        LOGGER.info("Getting all dislikes");
         return dislikePostService.getAllDislikePosts();
     }
 }
